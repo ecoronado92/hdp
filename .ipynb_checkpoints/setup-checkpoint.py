602173ba571cfc5b5ddf977b144fc8fb68eb9e64
Andrew Crar <andrew.carr@duke.edu>
2020-04-21 15:00:22 -0400

    update

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hplda',
+      version='0.1',
+      description='Implementation of Hierarchical LDA',
+      url='http://github.com/datadiarist/hplda',
+      author='Andrew and Eduardo',
+      author_email='andrew.carr@duke.edu',
+      license='MIT',
+      packages=['hplda'],
+      zip_safe=False)
